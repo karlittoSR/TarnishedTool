5486fbbdbfa7d501c710569eace96f13c9008843
perunavich <125222551+perunavich@users.noreply.github.com>
2026-07-06 18:45:43 +0200

    Clear hook registry on detach (#118)

--- TarnishedTool/Memory/HookManager.cs
+++ TarnishedTool/Memory/HookManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using TarnishedTool.Enums;
 using TarnishedTool.Interfaces;
 using TarnishedTool.Services;
 
@@ -21,6 +22,7 @@ namespace TarnishedTool.Memory
         public HookManager(IMemoryService memoryService, IStateService stateService)
         {
             _memoryService = memoryService;
+            stateService.Subscribe(State.Detached, ClearHooks);
         }
         
         public bool IsHookInstalled(long key) =>  _hookRegistry.ContainsKey(key);
